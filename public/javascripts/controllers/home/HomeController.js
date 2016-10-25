cf827d94fb271506c2e4a3fc38accf416863c5fa
zhangjk95 <zjk_261011@163.com>
2016-10-25 18:49:34 +0800

    fix animation in home page

--- public/javascripts/controllers/home/HomeController.js
+++ public/javascripts/controllers/home/HomeController.js
@@ -5,6 +5,6 @@ angular.module('QuestClear').controller("HomeController", function ($scope) {
         var offsetX = $event.pageX - homeBg0.offset().left - homeBg0.width() / 2;
         var offsetY = $event.pageY - homeBg0.offset().top - homeBg0.height() / 2;
 
-        homeBg0.css('transform', 'scale(1.02, 1.02) translate(' + Math.floor(offsetX * -0.01) + 'px, ' + Math.floor(offsetY * -0.01) + 'px)');
+        homeBg0.css('transform', 'scale(1.02, 1.02) rotateX(' + offsetY * 0.00075 + 'deg) rotateY(' + -offsetX * 0.0005 + 'deg)');
     }
 });
