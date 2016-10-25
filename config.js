9767607c70bf2cb4a36b4d300b4f938cc0c76390
zhangjk95 <zjk_261011@163.com>
2016-10-25 13:57:29 +0800

    clear mock list

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = {
     backendUrlPrefix: 'http://localhost:4000',
     mockUrlPrefix: 'http://localhost:3000/mock',
     mockList: [
-        '/login',
-        '/logout',
-        '/auth',
-        '/users(/.*)?'
+        // '/login',
+        // '/logout',
+        // '/auth',
+        // '/users(/.*)?'
     ]
 };
